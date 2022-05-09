1aa4c580e4c43446043d48840e4cfe9ed9a9ea1e
Dhaval Jani <56317794+dhavaljjani@users.noreply.github.com>
2022-05-09 13:59:19 -0700

    created league select file

---
+++ league-select.js
@@ -0,0 +1,24 @@
+//What league is currently selected in the dropdown?
+function leagueSelect(){
+    var leagueName = document.getElementById("leagues-select").value;
+	var codeNum = null;
+	switch (leagueName){
+		case("premier-league"):
+			codeNum = 0;
+			break;
+		case("la-liga"):
+			codeNum = 1;
+			break;
+		case("bundesliga"):
+			codeNum = 2;
+			break;
+		case("ligue-1"):
+			codeNum = 3;
+			break;
+		case("serie-a"):
+			console.log("YOU WERE CLEARLY MEANT FOR MORE");
+			codeNum = 4;
+			break;
+	}
+    return codeNum;
+}
