255b7d030e18ee642d431e19f66f4d30ae05a449
Dhaval Jani <56317794+dhavaljjani@users.noreply.github.com>
2022-05-30 00:37:11 -0700

    API token error handling + MLS stuff

--- popup.js
+++ popup.js
@@ -57,10 +57,31 @@ function makeWikipediaLink(teamName){
 	return url;
 }
 
+function makeAPIinputElement(){
+	var a = document.createElement("a")
+	a.href = "https://www.football-data.org/";
+
+	var APIinput = document.createElement("input");
+	APIinput.type = "text";
+	APIinput.id = "APIinput";
+
+	var linkText = document.createTextNode("https://www.football-data.org/");
+    a.appendChild(linkText);
+    a.href = "https://www.football-data.org/";
+
+	document.getElementById("APItext").innerHTML += "If you would like to make more than 10 calls per minute, feel free to create an account on ";
+	document.getElementById("APItext").appendChild(a);
+	document.getElementById("APItext").innerHTML += " and create your own API token with varying plans. Copy and paste it here to use: ";
+	document.getElementById("APItext").appendChild(APIinput);
+}
+
 function returnMatches(){
 	//clear divs
 	document.getElementById('matches').innerHTML = "";
-		
+	document.getElementById("APItext").innerHTML = "";
+	
+	makeAPIinputElement();
+	
 	//This code gets the value of the league selector ui and converts it to a number we can use to correspond with football data api
 	var leagueCode = leagueSelect();
 	var numMatchesToDisplay = document.getElementById("numMatches").value;
@@ -146,14 +167,81 @@ function returnMatches(){
 }
 
 function returnStandings(){
-	//clear standings1 id div:
-	document.getElementById('standings1').innerHTML = "";
+	if (document.getElementById("APIinput")){
+		if (document.getElementById("APIinput").value != ""){
+			API_KEY = document.getElementById("APIinput").value;
+		} else {
+			API_KEY = "95302cdd6b314a0a834875835cdd7a15";
+		}
+	}
+	document.getElementById("APItext").innerHTML = "";
+
+	makeAPIinputElement();
+
+	//clear standings1 id div: 
+	while (document.getElementById('standings1').firstChild) {
+		if(document.getElementById('standings1').firstChild.id != "yearSelect"){
+			document.getElementById('standings1').removeChild(document.getElementById('standings1').firstChild);
+		}
+    }
 
 	var leagueCode = leagueSelect();
 	if(leagueCode == -1){
-		//for MLS data
-		const xhttps = new XMLHttpRequest();
-		xhttps.addEventListener("load", function onLoad () {
+		document.getElementById("oldMLSdata").style.visibility = "visible";
+		var year = document.getElementById("standingsYear").value;
+		if(year != "2022"){
+
+			const xhttps = new XMLHttpRequest();
+			xhttps.addEventListener("load", function onLoad () {
+				var parser = new DOMParser();
+				var htmlDoc = parser.parseFromString(this.responseText, 'text/html');
+				
+				if(htmlDoc.querySelector("#mw-content-text > div.mw-parser-output > table > tbody > tr:nth-child(1) > th:nth-child(2)") != null){
+					htmlDoc.querySelector("#mw-content-text > div.mw-parser-output > table > tbody > tr:nth-child(1) > th:nth-child(2)").innerHTML = "";
+				}
+
+
+				table = htmlDoc.getElementsByClassName('wikitable')[0]; //only table in page (index 0 of HTML Collection)
+				console.log(table);
+
+				var label = document.createElement("p");
+				label.style.backgroundColor = "#212529";
+				label.style.color = "white";
+				document.getElementById("standings1").innerHTML += "<br>";
+				label.innerHTML = "Eastern Conference Standings";
+				document.getElementById("standings1").appendChild(label);
+				document.getElementById("standings1").appendChild(table);
+			});
+			xhttps.open("GET", "https://en.wikipedia.org/wiki/Template:" + year + "_Major_League_Soccer_Eastern_Conference_table");
+			xhttps.send();
+
+			const xhttps2 = new XMLHttpRequest();
+			xhttps2.addEventListener("load", function onLoad () {
+				var parser = new DOMParser();
+				var htmlDoc = parser.parseFromString(this.responseText, 'text/html');
+
+				if(htmlDoc.querySelector("#mw-content-text > div.mw-parser-output > table > tbody > tr:nth-child(1) > th:nth-child(2)") != null){
+					htmlDoc.querySelector("#mw-content-text > div.mw-parser-output > table > tbody > tr:nth-child(1) > th:nth-child(2)").innerHTML = "";
+				}
+
+
+				table = htmlDoc.getElementsByClassName('wikitable')[0]; //only table in page (index 0 of HTML Collection)
+				console.log(table);
+
+				var label = document.createElement("p");
+				label.style.backgroundColor = "#212529";
+				label.style.color = "white";
+				document.getElementById("standings1").innerHTML += "<br>";
+				label.innerHTML = "Western Conference Standings";
+				document.getElementById("standings1").appendChild(label);
+				document.getElementById("standings1").appendChild(table);
+			});
+			xhttps2.open("GET", "https://en.wikipedia.org/wiki/Template:" + year + "_Major_League_Soccer_Western_Conference_table");
+			xhttps2.send();
+		} else {
+			//for MLS data
+			const xhttps = new XMLHttpRequest();
+			xhttps.addEventListener("load", function onLoad () {
 			var parser = new DOMParser();
 			var htmlDoc = parser.parseFromString(this.responseText, 'text/html');
 
@@ -199,66 +287,73 @@ function returnStandings(){
 			//Empty TD for lower standing rows is changed to white for formatting
 			document.querySelector("#standings1 > table:nth-child(3) > tbody > tr:nth-child(9) > td:nth-child(11)").style = "background-color: white;";
 			document.querySelector("#standings1 > table:nth-child(6) > tbody > tr:nth-child(9) > td:nth-child(11)").style = "background-color: white;";
-		});
-		xhttps.open("GET", "https://en.wikipedia.org/wiki/Template:2022_Major_League_Soccer_Eastern_Conference_table");
-		xhttps.send();
+			});
+			xhttps.open("GET", "https://en.wikipedia.org/wiki/Template:2022_Major_League_Soccer_Eastern_Conference_table");
+			xhttps.send();
 
-		const xhttps2 = new XMLHttpRequest();
-		xhttps2.addEventListener("load", function onLoad () {
-			var parser = new DOMParser();
-			var htmlDoc = parser.parseFromString(this.responseText, 'text/html');
+			const xhttps2 = new XMLHttpRequest();
+			xhttps2.addEventListener("load", function onLoad () {
+				var parser = new DOMParser();
+				var htmlDoc = parser.parseFromString(this.responseText, 'text/html');
 
-			htmlDoc.querySelector("#mw-content-text > div.mw-parser-output > table > tbody > tr:nth-child(1) > th:nth-child(2)").innerHTML = "";
+				htmlDoc.querySelector("#mw-content-text > div.mw-parser-output > table > tbody > tr:nth-child(1) > th:nth-child(2)").innerHTML = "";
 
-			table = htmlDoc.getElementsByClassName('wikitable')[0]; //only table in page (index 0 of HTML Collection)
+				table = htmlDoc.getElementsByClassName('wikitable')[0]; //only table in page (index 0 of HTML Collection)
 
-			var color = "";
-			for(var standingIndex = 2 ; standingIndex <= table.rows.length ; standingIndex++){
-				if (standingIndex == 2){
-					color = "background-color: #1C9A1E;";
-				} else if(standingIndex <= 8){
-					color = "background-color: #58D859;";
-				} else {
-					color = "background-color: white;";
-				}
-				htmlDoc.querySelector("#mw-content-text > div.mw-parser-output > table > tbody > tr:nth-child(" + standingIndex + ") > th > a").removeAttribute("href");
-				for(var td = 1 ; td <= 11 ; td++){
-					if(td == 2){
-						htmlDoc.querySelector("#mw-content-text > div.mw-parser-output > table > tbody > tr:nth-child(" + standingIndex + ") > th").style = color;
-					} else if(td == 11){
-						if(standingIndex == 2 || standingIndex == 3){
+				var color = "";
+				for(var standingIndex = 2 ; standingIndex <= table.rows.length ; standingIndex++){
+					if (standingIndex == 2){
+						color = "background-color: #1C9A1E;";
+					} else if(standingIndex <= 8){
+						color = "background-color: #58D859;";
+					} else {
+						color = "background-color: white;";
+					}
+					htmlDoc.querySelector("#mw-content-text > div.mw-parser-output > table > tbody > tr:nth-child(" + standingIndex + ") > th > a").removeAttribute("href");
+					for(var td = 1 ; td <= 11 ; td++){
+						if(td == 2){
+							htmlDoc.querySelector("#mw-content-text > div.mw-parser-output > table > tbody > tr:nth-child(" + standingIndex + ") > th").style = color;
+						} else if(td == 11){
+							if(standingIndex == 2 || standingIndex == 3){
+								htmlDoc.querySelector("#mw-content-text > div.mw-parser-output > table > tbody > tr:nth-child(" + standingIndex + ") > td:nth-child(" + td + ")").style = color;
+							}
+						} else {
 							htmlDoc.querySelector("#mw-content-text > div.mw-parser-output > table > tbody > tr:nth-child(" + standingIndex + ") > td:nth-child(" + td + ")").style = color;
 						}
-					} else {
-						htmlDoc.querySelector("#mw-content-text > div.mw-parser-output > table > tbody > tr:nth-child(" + standingIndex + ") > td:nth-child(" + td + ")").style = color;
 					}
 				}
-			}
 
-			for(var k = 1; k < table.rows.length ; k++){
-				console.log(table.rows[k])
-			}
+				for(var k = 1; k < table.rows.length ; k++){
+					console.log(table.rows[k])
+				}
 
-			var label = document.createElement("p");
-			label.style.backgroundColor = "#212529";
-			label.style.color = "white";
-			document.getElementById("standings1").innerHTML += "<br>";
-			label.innerHTML = "Western Conference Standings";
-			document.getElementById("standings1").appendChild(label);
-			document.getElementById("standings1").appendChild(table);
+				var label = document.createElement("p");
+				label.style.backgroundColor = "#212529";
+				label.style.color = "white";
+				document.getElementById("standings1").innerHTML += "<br>";
+				label.innerHTML = "Western Conference Standings";
+				document.getElementById("standings1").appendChild(label);
+				document.getElementById("standings1").appendChild(table);
 
-			//Empty TD for lower standing rows is changed to white for formatting
-			document.querySelector("#standings1 > table:nth-child(3) > tbody > tr:nth-child(9) > td:nth-child(11)").style = "background-color: white;";
-			document.querySelector("#standings1 > table:nth-child(6) > tbody > tr:nth-child(9) > td:nth-child(11)").style = "background-color: white;";
-		});
-		xhttps2.open("GET", "https://en.wikipedia.org/wiki/Template:2022_Major_League_Soccer_Western_Conference_table");
-		xhttps2.send();
+				//Empty TD for lower standing rows is changed to white for formatting
+				document.querySelector("#standings1 > table:nth-child(3) > tbody > tr:nth-child(9) > td:nth-child(11)").style = "background-color: white;";
+				document.querySelector("#standings1 > table:nth-child(6) > tbody > tr:nth-child(9) > td:nth-child(11)").style = "background-color: white;";
+			});
+			xhttps2.open("GET", "https://en.wikipedia.org/wiki/Template:2022_Major_League_Soccer_Western_Conference_table");
+			xhttps2.send();
+		}
 	} else {
+		document.getElementById("oldMLSdata").style.visibility = "hidden";
 		$.ajax({
 			headers: { 'X-Auth-Token': API_KEY },
 			url: "https://api.football-data.org/v2/competitions/" + competitionCode[leagueCode] + "/standings",
 			dataType: 'json',
 			type: 'GET',
+			error: function(){
+				var errorText = document.createElement("p");
+				errorText.innerHTML = "Invalid API token entered! Try new token, or reload extension.";
+				document.getElementById("standings1").appendChild(errorText);
+			}
 		}).done(function(response) {
 			for(var j = 0 ; j < response.standings.length ; j++){
 				let standingsTable = document.createElement("table");
